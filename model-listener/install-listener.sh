1d870c626e4722a54f53f4aace5ad819d0be7e60
Forrest Milner <fmilner@zoom456.com>
2024-02-23 15:20:02 -0500

    Fixed installers

--- model-listener/install-listener.sh
+++ model-listener/install-listener.sh
@@ -3,9 +3,7 @@
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc
 nvm install 16
-npm init
+
 npm install firebase
 npm install node-fetch
-sudo cp /home/jetson/jetson-ml-client/services/* /etc/systemd/system/
-sudo cp /home/jetson/jetson-ml-client/service_scripts/* /usr/bin/
 
--- services/install_services.sh
+++ services/install_services.sh
@@ -1,6 +1,7 @@
+sudo cp /home/jetson/jetson-ml-client/services/* /etc/systemd/system/
+sudo cp /home/jetson/jetson-ml-client/service_scripts/* /usr/bin/
+
 sudo systemctl enable enes100ml.service
 sudo systemctl enable mlmodelpull.service
 sudo systemctl start enes100ml.service
 sudo systemctl start mlmodelpull.service
-sudo cp /home/jetson/jetson-ml-client/services/* /etc/systemd/system/
-sudo cp /home/jetson/jetson-ml-client/service_scripts/* /usr/bin/
