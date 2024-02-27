1ffbd4fb71bfe1fc95ceec63459de67e972e7a1a
Joshua Stone <99224714+jeronstone@users.noreply.github.com>
2024-02-26 19:04:19 -0500

    Update install-listener.sh

--- model-listener/install-listener.sh
+++ model-listener/install-listener.sh
@@ -4,6 +4,7 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc
 nvm install 16
 
+npm install -g npm@latest
 npm install firebase
 npm install node-fetch
 
